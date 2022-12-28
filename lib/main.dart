cc079da0c3cd3a7d4de281290e5527602520040a
Hong Sunghwi <tnfy10@gmail.com>
2022-12-28 21:55:48 +0900

    Go Router 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:go_router_example/screen/home_screen.dart';
 
 void main() {
@@ -6,12 +7,30 @@ void main() {
 }
 
 class _App extends StatelessWidget {
-  const _App({super.key});
+  const _App();
+
+  GoRouter get _router => GoRouter(
+        initialLocation: '/',
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (_, state) => const HomeScreen(),
+          )
+        ],
+      );
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp.router(
+      // 라우트 정보를 전달
+      routeInformationProvider: _router.routeInformationProvider,
+      // URI String을 상태 및 Go Router에서 사용할 수 있는 형태로
+      // 변환해주는 함수
+      routeInformationParser: _router.routeInformationParser,
+      // 위에서 변경된 값으로
+      // 실제 어떤 라우트를 보여줄지
+      // 정하는 함수
+      routerDelegate: _router.routerDelegate,
     );
   }
 }
