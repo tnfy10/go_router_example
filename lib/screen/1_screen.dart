81281ee7bd85d270516d6aede63c1dbaa06f5c8f
Hong Sunghwi <tnfy10@gmail.com>
2022-12-28 23:13:07 +0900

    pop 함수

--- lib/screen/1_screen.dart
+++ lib/screen/1_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../layout/default_layout.dart';
 
@@ -9,8 +10,14 @@ class OneScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-
+          ElevatedButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: const Text('POP'),
+          ),
         ],
       ),
     );
